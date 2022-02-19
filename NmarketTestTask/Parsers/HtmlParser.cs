613c178358522802e9c2c13fadec8474f14d68ee
Кристина Сюник <anitanit43@gmail.com>
2022-02-19 22:49:51 +0300

    houses deserialization

--- NmarketTestTask/Parsers/HtmlParser.cs
+++ NmarketTestTask/Parsers/HtmlParser.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using HtmlAgilityPack;
 using NmarketTestTask.Models;
 
@@ -8,15 +11,64 @@ namespace NmarketTestTask.Parsers
     {
         public IList<House> GetHouses(string path)
         {
+            List<House> houses = new List<House>();
+            House currentHouse;
             var doc = new HtmlDocument();
             doc.Load(path);
 
-            //Примеры использования библиотек
-            var node = doc.DocumentNode.SelectSingleNode("//thead");
-            var text = node.InnerText;
-            var nodes = doc.DocumentNode.SelectNodes(".//th");
 
-            throw new System.NotImplementedException();
+            var deserialised = XDocument.Parse(doc.ParsedText)
+                .Descendants("html")
+                .Descendants("body")
+                .Descendants("table")
+                .Descendants("tbody")
+                .Descendants("tr")
+                .ToList();
+
+            foreach (var line in deserialised)
+            {
+                var houseFound = houses.FirstOrDefault(x => x.Name == line.Elements()
+                                                                          .FirstOrDefault(h => h.FirstAttribute.Value == ("house")).Value);
+                if (houseFound == null)
+                {
+                    currentHouse = CreateNewHouse(line);
+                    houses.Add(currentHouse);
+                }
+                else
+                {
+                    var flatFound = houseFound.Flats.FirstOrDefault(x => x.Number == line.Elements()
+                                                                                         .FirstOrDefault(h => h.FirstAttribute.Value == ("number")).Value);
+                    if (flatFound == null)
+                    {
+                        houseFound.Flats.Add(new Flat()
+                        {
+                            Number = line.Elements().First(x => x.FirstAttribute.Value == "number").Value,
+                            Price = line.Elements().First(x => x.FirstAttribute.Value == "price").Value
+                        });
+                    }
+                }
+
+            }
+
+            return houses;
+        }
+
+        private static House CreateNewHouse(XElement element)
+        {
+            var elements = element.Elements("td").ToList();
+            var name = elements.FirstOrDefault(x => x.FirstAttribute.Value == "house");
+            return new House()
+            {
+                Name = name.Value,
+                Flats = new List<Flat>
+                {
+                    new Flat()
+                    {
+                        Number = elements.FirstOrDefault(x => x.FirstAttribute.Value =="number").Value,
+                        Price = elements.FirstOrDefault(x => x.FirstAttribute.Value =="price").Value
+                    }
+                }
+            };
         }
     }
 }
