c06fb433a05b435b71419e8e098f05a9b1942133
Кристина Сюник <anitanit43@gmail.com>
2022-02-20 00:49:36 +0300

    ExcelParser

--- NmarketTestTask/Parsers/ExcelParser.cs
+++ NmarketTestTask/Parsers/ExcelParser.cs
@@ -10,17 +10,68 @@ namespace NmarketTestTask.Parsers
     {
         public IList<House> GetHouses(string path)
         {
+            List<House> houses = new List<House>();
+            House currentHouse = null;
+
             var workbook = new XLWorkbook(path);
             var sheet = workbook.Worksheets.First();
 
-            //Примеры использования библиотек
-            var cell = sheet.Cell(1, 1);
-            var row = cell.WorksheetRow().RowNumber();
-            var column = cell.WorksheetColumn().ColumnNumber();
-            var value = cell.GetValue<string>();
-            var cells = sheet.Cells().Where(c => c.GetValue<string>().Contains("1")).ToList();
-            
-            throw new NotImplementedException();
+            var cellsVal = sheet.Cells().Where(c => c.GetValue<string>() != "").ToList();
+
+            var rows = sheet.Rows();
+            foreach (var cell in cellsVal)
+            {
+                var value = cell.GetValue<string>();
+                if (value.Contains("Дом"))
+                {
+                    if (currentHouse != null)
+                    {
+                        houses.Add(currentHouse);
+                        currentHouse = null;
+                    }
+
+                    currentHouse = new House()
+                    {
+                        Name = value,
+                        Flats = new List<Flat>()
+                    };
+                }
+
+                if (value.Contains("№"))
+                {
+                    if (currentHouse != null)
+                    {
+                        var row = cell.WorksheetRow().RowNumber();
+                        var column = cell.WorksheetColumn().ColumnNumber();
+                        if (currentHouse.Flats.Count != 0)
+                        {
+                            currentHouse.Flats.Add(new Flat()
+                            {
+                                Number = value,
+                                Price = sheet.Cell(row + 1, column).GetValue<string>()
+                            });
+                        }
+                        else
+                        {
+                            currentHouse.Flats = new List<Flat>
+                            {
+                                new Flat()
+                                {
+                                    Number = value,
+                                    Price = sheet.Cell(row + 1, column).GetValue<string>()
+                                }
+                            };
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("На странице не нашлось имени дома");
+                    }
+                }
+            }
+            houses.Add(currentHouse);
+
+            return houses;
         }
     }
 }
